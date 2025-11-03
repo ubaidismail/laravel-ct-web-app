f8eff063eaa44bcb12a296b66738458c3f5852b4
Ubaid Ismail <ubaidismail378@gmail.com>
2025-11-04 04:54:38 +0500

    feat: Add mobile device warning for proposal view

--- resources/views/filament/resources/proposal-resource/pages/proposal-builder.blade.php
+++ resources/views/filament/resources/proposal-resource/pages/proposal-builder.blade.php
@@ -1,4 +1,18 @@
 <x-filament-panels::page>
+{{-- Mobile Warning Message - Shows only on mobile/tablet --}}
+    <div id="mobile-warning" style="display: none;" class="bg-yellow-50 border-2 border-yellow-300 rounded-lg p-6 mb-6 mx-4">
+        <div class="flex items-start gap-4">
+            <svg class="w-8 h-8 text-yellow-600 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
+            </svg>
+            <div>
+                <h3 class="text-lg font-bold text-yellow-800 mb-2">Mobile Device Detected</h3>
+                <p class="text-yellow-700 font-medium">
+                    For the best viewing and signing experience, please open this proposal on a desktop or laptop computer.
+                </p>
+            </div>
+        </div>
+    </div>
     <style>
         /* Base Styles */
         .custom_sign_btn {
